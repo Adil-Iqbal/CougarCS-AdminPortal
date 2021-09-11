bdb96eb01a3d361282768509556c245c93d04ac0
Adil Iqbal <aiqbal85@gmail.com>
2021-09-11 00:22:00 -0500

    setup linting

--- logger/index.ts
+++ logger/index.ts
@@ -5,15 +5,24 @@ import crypto from 'crypto';
 const startTime = new Date().toISOString();
 
 const cloudWatchTransport = new WinstonCloudwatch({
-    name: "cougarcs-admin-portal",
-    logGroupName: 'admin-portal',
-    logStreamName: function() {
-      let date = new Date().toISOString().split('T')[0];
-      return 'admin-portal-' + date + '-' + crypto.createHash('md5')
-        .update(startTime)
-        .digest('hex');
-    }
-  });
+  name: 'cougarcs-admin-portal',
+  logGroupName: 'admin-portal',
+  logStreamName() {
+    const date = new Date().toISOString().split('T')[0];
+    return `prod-${date}-${crypto
+      .createHash('md5')
+      .update(startTime)
+      .digest('hex')}`;
+  },
+});
 
-  const consoleTransport = new winston.transports.Console({ level: 'info' });
+const consoleTransport = new winston.transports.Console({ level: 'info' });
 
+winston.loggers.add('prod-logger', {
+  transports: [
+    cloudWatchTransport,
+    consoleTransport
+  ]
+});
+
+export default winston.loggers.get('prod-logger');
